47a7b3986917506267fc9f1a4bc0e241c7c3f0a5
Leila Ybanez <leilanybanez@gmail.com>
2024-04-17 23:08:53 -0300

    chore: complete ShareView component layout and styles

--- app/components/ShareView.tsx
+++ app/components/ShareView.tsx
@@ -1,12 +1,16 @@
 import styles from "../styles/shareView.module.css";
 import ActionCard from "./ActionCard";
+import ValueCard from "./ValueCard";
 
 export default function ShareView() {
   return (
     <section className={styles.container}>
-      <h3>Acciones Coca-Cola</h3>
-      <ShareView />
-      <ActionCard />
+      <h3 className={styles.title}>Acciones Coca-Cola</h3>
+      <ValueCard />
+      <section className={styles.cardsContainer}>
+        <ActionCard action="Comprar" />
+        <ActionCard action="Vender" />
+      </section>
     </section>
   );
 }
