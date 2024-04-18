c5ca3aaa3879d167019c2af78b2b16c8151d3418
Leila Ybanez <leilanybanez@gmail.com>
2024-04-17 23:07:55 -0300

    feat: create ValueCard component and styles

--- app/components/ValueCard.tsx
+++ app/components/ValueCard.tsx
@@ -1,9 +1,11 @@
+import styles from "../styles/valueCard.module.css";
+
 export default function ValueCard() {
   return (
-    <section>
-      <h4>Valores</h4>
-      <hr />
-      <ul>
+    <section className={styles.container}>
+      <h4 className={styles.title}>Valores</h4>
+      <hr className={styles.divider}/>
+      <ul className={styles.list}>
         <li>Cantidad: 200 unidades</li>
         <li>Cotizacion: AR$ 2000/unidad</li>
         <li>Valor actual: AR$ 400.000</li>
