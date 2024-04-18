97c1535c6c6a8c5a519575d18a71bfa0fe032641
Leila Ybanez <leilanybanez@gmail.com>
2024-04-17 23:07:18 -0300

    feat: create ActionCard component and styles

--- app/components/ActionCard.tsx
+++ app/components/ActionCard.tsx
@@ -1,8 +1,11 @@
-export default function ActionCard() {
+import styles from "../styles/actionCard.module.css";
+
+export default function ActionCard({ action }) {
   return (
-    <section>
-      <input type="number" /> <span> = AR$ 200.000 </span>
-      <button>Comprar</button>
+    <section className={styles.container}>
+      <input type="number" min={0} className={styles.input} />{" "}
+      <span className={styles.span}> = AR$ 200.000 </span>
+      <button className={styles.button}>{action}</button>
     </section>
   );
 }
