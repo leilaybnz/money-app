7992c655543ee7ca0362b70e60054966dc2bd954
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 13:40:27 -0300

    chore: add props interface and render temporary props, fix font size

--- app/components/ShareView.tsx
+++ app/components/ShareView.tsx
@@ -1,12 +1,17 @@
+import { Share } from "~/types";
 import styles from "../styles/shareView.module.css";
 import ActionCard from "./ActionCard";
 import ValueCard from "./ValueCard";
 
-export default function ShareView() {
+interface ShareViewProps {
+  share: Share;
+}
+
+export default function ShareView({ share }: ShareViewProps) {
   return (
     <section className={styles.container}>
-      <h3 className={styles.title}>Acciones Coca-Cola</h3>
-      <ValueCard />
+      <h3 className={styles.title}>Acciones {share.name}</h3>
+      <ValueCard share={share} />
       <section className={styles.cardsContainer}>
         <ActionCard action="Comprar" />
         <ActionCard action="Vender" />
