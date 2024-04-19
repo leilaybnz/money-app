563bf024c117a80abb0ba82d2b847681e65bf744
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 13:51:39 -0300

    chore: add props interface, return props and fix bg color

--- app/components/ValueCard.tsx
+++ app/components/ValueCard.tsx
@@ -1,14 +1,19 @@
+import { Share } from "~/types";
 import styles from "../styles/valueCard.module.css";
 
-export default function ValueCard() {
+interface ValueCardProps {
+  share: Share;
+}
+
+export default function ValueCard({ share }: ValueCardProps) {
   return (
     <section className={styles.container}>
       <h4 className={styles.title}>Valores</h4>
-      <hr className={styles.divider}/>
+      <hr className={styles.divider} />
       <ul className={styles.list}>
-        <li>Cantidad: 200 unidades</li>
-        <li>Cotizacion: AR$ 2000/unidad</li>
-        <li>Valor actual: AR$ 400.000</li>
+        <li>Cantidad: {share.quantity} unidades</li>
+        <li>Cotizacion: AR$ {share.price}/unidad</li>
+        <li>Valor actual: AR$ {share.currentValue}</li>
       </ul>
     </section>
   );
