fcd781d672cc22782d393997c5de4c90aea7f46f
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 20:38:37 -0300

    chore: renaming and mapping of money

--- app/components/TotalValue.tsx
+++ app/components/TotalValue.tsx
@@ -1,18 +1,18 @@
-import { Savings } from "~/types";
+import { Money } from "~/types";
 import styles from "../styles/totalValue.module.css";
 
-interface TotalValueProps {
-  savings: Savings;
+interface TotalValuesProps {
+  money: Money[];
 }
 
-export default function TotalValue({ savings }: TotalValueProps) {
-  return (
-    <section className={styles.container}>
+export default function TotalValues({ money }: TotalValuesProps) {
+  return money.map((money) => (
+    <section className={styles.container} key={money.currency}>
       <h2 className={styles.title}>
-        Valor total cartera: {savings.name}
-        {" " + savings.quantity}
+        Valor total cartera: {money.currency}
+        {" " + money.amount}
       </h2>
       <img src="../../public/mock.png" alt="grafico" className={styles.img} />
     </section>
-  );
+  ));
 }
