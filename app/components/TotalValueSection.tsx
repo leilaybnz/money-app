8c4e15324263f795b4239cf88b5eb8dc87bb8021
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 22:41:09 -0300

    chore: add and render shares prop

--- app/components/TotalValueSection.tsx
+++ app/components/TotalValueSection.tsx
@@ -1,14 +1,18 @@
 import TotalValue from "./TotalValue";
-import { Money } from "~/types";
+import { Money, Share } from "~/types";
 
 interface TotalValueSectionProps {
   money: Money[];
+  shares: Share[];
 }
 
-export default function TotalValueSection({ money }: TotalValueSectionProps) {
+export default function TotalValueSection({
+  money,
+  shares,
+}: TotalValueSectionProps) {
   return (
     <main>
-      <TotalValue money={money} />
+      <TotalValue money={money} shares={shares} />
     </main>
   );
 }
