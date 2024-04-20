534465eb026fda8201cd4fe0f45299eaef129221
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 22:47:46 -0300

    chore: create loader data and add api calls

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,16 @@
 import type { MetaFunction } from "@remix-run/node";
 import Dashboard from "~/components/Dashboard";
+import { api } from "~/services/api";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Show me my money!" }];
 };
+
+export async function loader() {
+  const money = await api.savingsAccount.getMoney();
+  const shares = await api.savingsAccount.getShares();
+  return { money, shares };
+}
 
 export default function Index() {
   return <Dashboard />;
