7d8e54227c0dc71842d05f2f1e9df3626598a8d3
Leila Ybanez <leilanybanez@gmail.com>
2024-04-19 22:40:22 -0300

    chore: import Table

--- app/components/TotalValue.tsx
+++ app/components/TotalValue.tsx
@@ -1,18 +1,20 @@
-import { Money } from "~/types";
+import { Money, Share } from "~/types";
 import styles from "../styles/totalValue.module.css";
+import Table from "./Table";
 
 interface TotalValuesProps {
   money: Money[];
+  shares: Share[];
 }
 
-export default function TotalValues({ money }: TotalValuesProps) {
+export default function TotalValues({ money, shares }: TotalValuesProps) {
   return money.map((money) => (
     <section className={styles.container} key={money.currency}>
       <h2 className={styles.title}>
         Valor total cartera: {money.currency}
         {" " + money.amount}
       </h2>
-      <img src="../../public/mock.png" alt="grafico" className={styles.img} />
+      <Table money={money} shares={shares} />
     </section>
   ));
 }
