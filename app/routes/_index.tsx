06eda9b2c3d03606acb63ce681c2e54006a960ef
Leila Ybanez <leilanybanez@gmail.com>
2024-04-20 23:52:16 -0300

    chore: fetch on Promise all and import TotalValueSection

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,26 @@
 import type { MetaFunction } from "@remix-run/node";
 import Dashboard from "~/components/Dashboard";
 import { api } from "~/services/api";
-import { useLoaderData } from "@remix-run/react";
+import { json, useLoaderData } from "@remix-run/react";
+import TotalValueSection from "~/components/TotalValueSection";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Show me my money!" }];
 };
 
 export async function loader() {
-  const money = await api.savingsAccount.getMoney();
-  const shares = await api.savingsAccount.getShares();
-  return { money, shares };
+  const [money, shares] = await Promise.all([
+    api.savingsAccount.getMoney(),
+    api.savingsAccount.getShares(),
+  ]);
+  return json({ money, shares });
 }
 
 export default function Index() {
   const { money, shares } = useLoaderData<typeof loader>();
-  return <Dashboard money={money} shares={shares} />;
+  return (
+    <Dashboard shares={shares}>
+      <TotalValueSection money={money} shares={shares} />
+    </Dashboard>
+  );
 }
