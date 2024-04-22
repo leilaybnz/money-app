f2f7d117ac9509d5256562c5cf700835d195a60d
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:01:20 -0300

    feat: add state, import Form, render total amount and fix styles

--- app/components/BuyCard.tsx
+++ app/components/BuyCard.tsx
@@ -1,20 +1,35 @@
 import { Share } from "~/types";
 import styles from "../styles/actionCard.module.css";
+import { Form } from "@remix-run/react";
+import { useState } from "react";
 
 interface CardProps {
   share: Share;
 }
 
 export default function BuyCard({ share }: CardProps) {
+  const [amount, setAmount] = useState(0);
   return (
-    <section className={styles.container}>
-      <input type="number" min={0} className={styles.input} />{" "}
+    <Form className={styles.container} method="post">
+      <input
+        type="number"
+        name="amount"
+        min={0}
+        className={styles.input}
+        value={amount}
+        onChange={(event) => {
+          setAmount(parseInt(event.currentTarget.value));
+        }}
+      />{" "}
+      <input hidden name="_type" value="buy" />
       <span className={styles.span}>
         {" "}
         = {share.currency}
-        {share.price}{" "}
+        {Number.isNaN(amount) ? 0 : amount * share.price}
       </span>
-      <button className={styles.button}>Comprar</button>
-    </section>
+      <button className={styles.button} type="submit">
+        Comprar
+      </button>
+    </Form>
   );
 }
--- app/components/SellCard.tsx
+++ app/components/SellCard.tsx
@@ -1,20 +1,41 @@
 import { Share } from "~/types";
 import styles from "../styles/actionCard.module.css";
+import { Form, useActionData } from "@remix-run/react";
+import { useState } from "react";
+import { action } from "~/routes/$shareName";
 
 interface CardProps {
   share: Share;
 }
 
 export default function SellCard({ share }: CardProps) {
+  const [amount, setAmount] = useState(0);
+  const actionData = useActionData<typeof action>();
+
   return (
-    <section className={styles.container}>
-      <input type="number" min={0} className={styles.input} />{" "}
+    <Form className={styles.container} method="post">
+      <input
+        type="number"
+        name="amount"
+        min={0}
+        className={styles.input}
+        value={amount}
+        onChange={(event) => {
+          setAmount(parseInt(event.currentTarget.value));
+        }}
+      />{" "}
+      <input hidden name="_type" value="sell" />
       <span className={styles.span}>
         {" "}
         = {share.currency}
-        {share.price}{" "}
+        {Number.isNaN(amount) ? 0 : amount * share.price}
       </span>
-      <button className={styles.button}>Vender</button>
-    </section>
+      <button className={styles.button} type="submit">
+        Vender
+      </button>
+      {actionData?.error && (
+        <span className={styles.error}>{actionData.error}</span>
+      )}
+    </Form>
   );
 }
