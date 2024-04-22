eb302f9d0fa586805083ef4e02141dd9b2e113fe
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:23:37 -0300

    chore: render savings account amount and add filter to show only shares held

--- app/components/MyInvestments.tsx
+++ app/components/MyInvestments.tsx
@@ -1,12 +1,14 @@
 import { Link } from "@remix-run/react";
 import styles from "../styles/card.module.css";
-import { Share } from "~/types";
+import { Money, Share } from "~/types";
+import { formatMoney } from "~/utils";
 
 interface MyInvestmentsProps {
   shares: Share[];
+  money: Money[];
 }
 
-export default function MyInvestments({ shares }: MyInvestmentsProps) {
+export default function MyInvestments({ shares, money }: MyInvestmentsProps) {
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Mis inversiones</h3>
@@ -15,23 +17,28 @@ export default function MyInvestments({ shares }: MyInvestmentsProps) {
         <Link to={"/"} className={`${styles.link} ${styles.activeLink}`}>
           <li className={styles.item}>
             <p className={styles.paragraph}>Caja de ahorro:</p>{" "}
-            <span className={styles.span}>$200000</span>
+            <span className={styles.span}>
+              {money[0].currency}
+              {formatMoney(money[0].amount)}
+            </span>
           </li>
         </Link>
-        {shares.map((share) => (
-          <Link
-            to={`/${share.name}`}
-            key={share.name}
-            className={`${styles.link} ${styles.activeLink}`}
-          >
-            <li className={styles.item}>
-              <p className={styles.paragraph}>{share.name}:</p>{" "}
-              <span className={styles.span}>
-                {share.quantityOwned} unidades
-              </span>
-            </li>
-          </Link>
-        ))}
+        {shares
+          .filter((share) => share.quantityOwned > 0)
+          .map((share) => (
+            <Link
+              to={`/${share.name}`}
+              key={share.name}
+              className={`${styles.link} ${styles.activeLink}`}
+            >
+              <li className={styles.item}>
+                <p className={styles.paragraph}>{share.name}:</p>{" "}
+                <span className={styles.span}>
+                  {share.quantityOwned} unidades
+                </span>
+              </li>
+            </Link>
+          ))}
       </ul>
     </div>
   );
