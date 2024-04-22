8a1148e45a0c2f64ae74ae54f26079b3e2c5513d
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:02:00 -0300

    chore: add money prop and pass to Sidebar

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,17 +1,17 @@
-import { Share } from "~/types";
+import { Money, Share } from "~/types";
 import styles from "../styles/dashboard.module.css";
 import Sidebar from "./Sidebar";
 
 interface DashboardProps {
   children: React.ReactNode;
   shares: Share[];
+  money: Money[];
 }
 
-export default function Dashboard({ children, shares }: DashboardProps) {
+export default function Dashboard({ children, shares, money }: DashboardProps) {
   return (
     <main className={styles.container}>
-      <Sidebar shares={shares} />
-      {/* <pre>{JSON.stringify(money, null, 2)}</pre> */}
+      <Sidebar shares={shares} money={money} />
       {children}
     </main>
   );
