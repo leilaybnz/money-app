3938b02d6eed07ea2db15133788a5c765323799c
Leila Ybanez <leilanybanez@gmail.com>
2024-04-21 22:28:05 -0300

    feat: add action fn to fetch buyShares and sellShares, add money prop

--- app/routes/$shareName.tsx
+++ app/routes/$shareName.tsx
@@ -1,4 +1,9 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  json,
+  redirect,
+} from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Dashboard from "~/components/Dashboard";
 import ShareView from "~/components/ShareView";
@@ -11,9 +16,10 @@ export async function loader({ params }: LoaderFunctionArgs) {
       statusText: "Not Found",
     });
   }
-  const [share, shares] = await Promise.all([
+  const [share, shares, money] = await Promise.all([
     api.savingsAccount.getShare(params.shareName),
     api.savingsAccount.getShares(),
+    api.savingsAccount.getMoney(),
   ]);
 
   if (!share) {
@@ -22,13 +28,62 @@ export async function loader({ params }: LoaderFunctionArgs) {
       statusText: "Not Found",
     });
   }
-  return json({ share, shares });
+  return json({ share, shares, money });
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  console.log("action");
+  if (!params.shareName) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+
+  const body = await request.formData();
+  const amount = body.get("amount");
+  const type = body.get("_type");
+
+  if (type === "buy") {
+    if (!amount || typeof amount !== "string") {
+      throw new Response(null, {
+        status: 400,
+        statusText: "Amount is non existent",
+      });
+    }
+
+    await api.savingsAccount.buyShares(params.shareName, parseInt(amount));
+
+    return redirect("/");
+  }
+
+  if (type === "sell") {
+    if (!amount || typeof amount !== "string") {
+      throw new Response(null, {
+        status: 400,
+        statusText: "Amount is non existent",
+      });
+    }
+
+    const status = await api.savingsAccount.sellShares(
+      params.shareName,
+      parseInt(amount)
+    );
+
+    if (status.status === "FAILED") {
+      return json({ error: status.error });
+    }
+
+    return redirect("/");
+  }
+
+  throw new Error("Invalid action type");
 }
 
 export default function Share() {
-  const { share, shares } = useLoaderData<typeof loader>();
+  const { share, shares, money } = useLoaderData<typeof loader>();
   return (
-    <Dashboard shares={shares}>
+    <Dashboard shares={shares} money={money}>
       <ShareView share={share} />
     </Dashboard>
   );
