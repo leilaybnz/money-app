da5d2de86931905cc26342ccf6520d0130e0c52a
Leila Ybanez <leilanybanez@gmail.com>
2024-04-22 22:26:04 -0300

    chore: remove console log

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,7 +14,6 @@ export async function loader() {
     api.savingsAccount.getShares(),
     api.savingsAccount.getFullAmount(),
   ]);
-  console.log('f', fullAmount);
   return json({ money, shares, fullAmount });
 }
 
